ff6af30c2a431c6772f2189fb51ea0cc1c9af448
Michael Klein <mbklein@gmail.com>
2017-11-22 13:48:57 -0600

    Increase test coverage 💯

--- spec/support/dynamo.rb
+++ spec/support/dynamo.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
+  config.before(:suite) do
+    ignore_resource_error { dynamodb_client.delete_table(table_name: 'valkyrie_orm') }
+    ignore_resource_error { dynamodb_client.delete_table(table_name: 'valkyrie_orm.refs') }
+  end
+
   config.before do
     Valkyrie::Persistence::DynamoDB::MetadataAdapter.new(
       connection: dynamodb_client
@@ -7,7 +12,12 @@ RSpec.configure do |config|
   end
 end
 
+def ignore_resource_error
+  yield
+rescue Aws::DynamoDB::Errors::ResourceNotFoundException
+end
+
 def dynamodb_client
-  options = ENV['TEST_ON_AWS'] ? {} : { endpoint: 'http://localhost:8000' }
+  options = { endpoint: (ENV['DYNAMODB_ENDPOINT'] || 'http://localhost:8000') }
   Aws::DynamoDB::Client.new(options)
 end
--- spec/valkyrie/persistence/dynamodb/query_service_spec.rb
+++ spec/valkyrie/persistence/dynamodb/query_service_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Valkyrie::Persistence::DynamoDB::QueryService do
   let(:adapter) { Valkyrie::Persistence::DynamoDB::MetadataAdapter.new(connection: client) }
   let(:client)  { dynamodb_client }
   it_behaves_like "a Valkyrie query provider"
+
+  describe "invalid datetime" do
+    before do
+      class CustomResource < Valkyrie::Resource
+        attribute :id, Valkyrie::Types::ID.optional
+        attribute :foo
+      end
+    end
+    after do
+      Object.send(:remove_const, :CustomResource)
+    end
+
+    let(:resource) { adapter.persister.save(resource: CustomResource.new(foo: ['datetime-psych!'])) }
+    let(:reloaded_resource) { adapter.query_service.find_by(id: resource.id) }
+
+    it "falls through to the string handler" do
+      expect(reloaded_resource.foo).to eq(['datetime-psych!'])
+    end
+  end
 end
